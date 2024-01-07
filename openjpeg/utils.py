9ba6d189475719ecb5a5ca6674ebc9bd4040617a
scaramallion <scaramallion@users.noreply.github.com>
2024-01-07 17:22:01 +1100

    Cleanup types, formatting

--- openjpeg/utils.py
+++ openjpeg/utils.py
@@ -1,4 +1,3 @@
-
 from enum import IntEnum
 from io import BytesIO
 from math import ceil
@@ -115,7 +114,7 @@ def decode(
         If the decoding failed.
     """
     if isinstance(stream, (str, Path)):
-        with open(stream, 'rb') as f:
+        with open(stream, "rb") as f:
             buffer: BinaryIO = BytesIO(f.read())
             buffer.seek(0)
     elif isinstance(stream, (bytes, bytearray)):
@@ -152,7 +151,7 @@ def decode(
     arr = arr.view(dtype)
 
     shape = [rows, columns]
-    if pixels_per_sample> 1:
+    if pixels_per_sample > 1:
         shape.append(pixels_per_sample)
 
     return arr.reshape(*shape)
@@ -162,7 +161,7 @@ def decode_pixel_data(
     src: bytes,
     ds: Union["Dataset", Dict[str, Any], None] = None,
     version: int = Version.v1,
-    **kwargs: Any
+    **kwargs: Any,
 ) -> Union[np.ndarray, bytearray]:
     """Return the decoded JPEG 2000 data as a :class:`numpy.ndarray`.
 
@@ -212,14 +211,12 @@ def decode_pixel_data(
         samples_per_pixel = kwargs.get("samples_per_pixel")
         bits_stored = kwargs.get("bits_stored")
         pixel_representation = kwargs.get("pixel_representation")
-        no_kwargs = None in (
-            samples_per_pixel, bits_stored, pixel_representation
-        )
+        no_kwargs = None in (samples_per_pixel, bits_stored, pixel_representation)
 
         if not ds and no_kwargs:
             return cast(np.ndarray, arr)
 
-
+        ds = cast("Dataset", ds)
         samples_per_pixel = ds.get("SamplesPerPixel", samples_per_pixel)
         bits_stored = ds.get("BitsStored", bits_stored)
         pixel_representation = ds.get("PixelRepresentation", pixel_representation)
@@ -229,7 +226,7 @@ def decode_pixel_data(
             warnings.warn(
                 f"The (0028,0002) Samples per Pixel value '{samples_per_pixel}' "
                 f"in the dataset does not match the number of components "
-                f"\'{meta['nr_components']}\' found in the JPEG 2000 data. "
+                f"'{meta['nr_components']}' found in the JPEG 2000 data. "
                 f"It's recommended that you change the  Samples per Pixel value "
                 f"to produce the correct output"
             )
@@ -238,7 +235,7 @@ def decode_pixel_data(
             warnings.warn(
                 f"The (0028,0101) Bits Stored value '{bits_stored}' in the "
                 f"dataset does not match the component precision value "
-                f"\'{meta['precision']}\' found in the JPEG 2000 data. "
+                f"'{meta['precision']}' found in the JPEG 2000 data. "
                 f"It's recommended that you change the Bits Stored value to "
                 f"produce the correct output"
             )
@@ -297,7 +294,7 @@ def get_parameters(
         If reading the image parameters failed.
     """
     if isinstance(stream, (str, Path)):
-        with open(stream, 'rb') as f:
+        with open(stream, "rb") as f:
             buffer: BinaryIO = BytesIO(f.read())
             buffer.seek(0)
     elif isinstance(stream, (bytes, bytearray)):
