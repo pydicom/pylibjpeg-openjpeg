8c63bf95fefd85cf0a0a96f256dbabb133bc0dd3
Brad Hards <bradh@frogmouth.net>
2026-02-23 08:10:26 +1100

    Add support for adding PLT and TLM markers during encoding (#117)
    These can significantly improve decoding performance when pulling
    a small area out of a very large file, where the decoder supports it.
    
    OpenJPEG supports using the TLM marker as of https://github.com/uclouvain/openjpeg/pull/1538

--- openjpeg/_openjpeg.pyx
+++ openjpeg/_openjpeg.pyx
@@ -32,6 +32,8 @@ cdef extern int EncodeArray(
     PyObject* compression_ratios,
     PyObject* signal_noise_ratios,
     int codec_format,
+    bint add_tlm,
+    bint add_plt,
 )
 cdef extern int EncodeBuffer(
     PyObject* src,
@@ -46,6 +48,8 @@ cdef extern int EncodeBuffer(
     PyObject* compression_ratios,
     PyObject* signal_noise_ratios,
     int codec_format,
+    bint add_tlm,
+    bint add_plt,
 )
 
 
@@ -213,6 +217,8 @@ def encode_array(
     List[float] compression_ratios,
     List[float] signal_noise_ratios,
     int codec_format,
+    bint add_tlm,
+    bint add_plt,
 ) -> Tuple[int, bytes]:
     """Return the JPEG 2000 compressed `arr`.
 
@@ -239,6 +245,10 @@ def encode_array(
 
         * ``0``: JPEG 2000 codestream only (default) (J2K/J2C format)
         * ``1``: A boxed JPEG 2000 codestream (JP2 format)
+    add_tlm : bool
+        If ``True`` then add tile-part length markers (TLM) to the codestream.
+    add_plt : bool
+        If ``True`` then add packet length tile-part header markers (PLT) to the codestream.
 
     Returns
     -------
@@ -318,6 +328,8 @@ def encode_array(
         <PyObject *> compression_ratios,
         <PyObject *> signal_noise_ratios,
         codec_format,
+        add_tlm,
+        add_plt,
     )
     return return_code, dst.getvalue()
 
@@ -334,6 +346,9 @@ def encode_buffer(
     List[float] compression_ratios,
     List[float] signal_noise_ratios,
     int codec_format,
+    bint add_tlm,
+    bint add_plt,
+
 ) -> Tuple[int, bytes]:
     """Return the JPEG 2000 compressed `src`.
 
@@ -375,6 +390,10 @@ def encode_buffer(
 
         * ``0``: JPEG 2000 codestream only (default) (J2K/J2C format)
         * ``1``: A boxed JPEG 2000 codestream (JP2 format)
+    add_tlm : bool
+        If ``True`` then add tile-part length markers (TLM) to the codestream.
+    add_plt : bool
+        If ``True`` then add packet length tile-part header markers (PLT) to the codestream.
 
     Returns
     -------
@@ -466,5 +485,7 @@ def encode_buffer(
         <PyObject *> compression_ratios,
         <PyObject *> signal_noise_ratios,
         codec_format,
+        add_tlm,
+        add_plt,
     )
     return return_code, dst.getvalue()
